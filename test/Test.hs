abbab511e9d38d611c530bba4f8f24bd8075ff9d
Mark Grant <markrgrant@yahoo.com>
2016-03-14 20:24:04 -0700

    fix test example

--- test/Test.hs
+++ test/Test.hs
@@ -4,9 +4,9 @@ import Test.QuickCheck
 
 
 prop_revapp :: [Int] -> [Int] -> Bool
-prop_revap xs ys = reverse (xs++ys) == reverse xs ++ reverse ys
+prop_revapp xs ys = reverse (xs++ys) == reverse xs ++ reverse ys
 
 
 
 
-main = quickcheck prop_revapp
+main = quickCheck prop_revapp
