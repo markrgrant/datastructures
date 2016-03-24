7780a2cba483fb173b9bb5c3e2e707a416784560
Mark Grant <markrgrant@yahoo.com>
2016-03-23 18:54:53 -0700

    fix tests so that a proper exit value is detected upon test failure and returned

---
+++ test/DGraph/Test.hs
@@ -0,0 +1,39 @@
+module DGraph.Test where
+
+import Test.QuickCheck
+import qualified DGraph as G
+
+
+instance Arbitrary G.DGraph where
+    arbitrary = do
+        n <- choose (1,2) :: Gen Int
+        case n of
+            1 -> do -- create a graph with at least one vertex
+                numVertices <- choose (1, 100)
+                return $ G.create numVertices
+            2 -> do -- add an edge to a graph
+                g <- arbitrary
+                fr <- choose (0, (G.numVertices g) - 1)
+                to <- choose (0, (G.numVertices g) - 1)
+                return $ G.addEdge g fr to
+
+
+-- Adding an edge to a graph increases its edge count by 1
+prop_num_edges :: G.DGraph -> Bool
+prop_num_edges g = G.numEdges g' == (G.numEdges g + 1)
+    where g' = G.addEdge g 0 ((G.numVertices g) - 1)
+
+
+-- The number of vertices is the same as the number provided when creating the
+-- graph
+prop_num_vertices :: NonNegative Int -> Bool
+prop_num_vertices (NonNegative n) = G.numVertices g == n
+    where g = G.create n
+    
+
+-- Adding an edge from vertex i to vertex j makes vertex j
+-- adjacent to vertex i, but vertex i is not adjacent to vertex j
+prop_add_edge :: Bool
+prop_add_edge = (9 `elem` (G.adj g' 0)) && not (0 `elem` (G.adj g' 9))
+    where g = G.create 10
+          g' = G.addEdge g 0 9
--- test/Main.hs
+++ test/Main.hs
@@ -2,28 +2,38 @@
 
 import Test.QuickCheck
 import System.Exit (exitSuccess, exitFailure)
-import Control.Monad (liftM)
+import Control.Monad.Error
 
 import qualified Graph.Test
+import qualified DGraph.Test
 import qualified PQ.Test
 import qualified RBTree.Test
 import qualified DepthFirstPaths.Test
 
 
-import Data.List (foldl')
+type QuickCheckMonad = ErrorT String IO
 
+quickCheckResultW :: (Testable prop) => prop -> QuickCheckMonad Result
+quickCheckResultW prop = do
+    result <- liftIO $ quickCheckResult prop
+    case result of
+        (Success _ _ _) -> return result
+        (Failure {reason=str}) -> throwError str
 
-checkFailure :: Result -> Maybe ()
-checkFailure Failure {} = Nothing
-checkFailure _ = Just ()
 
 main :: IO ()
-main = quickCheckResult Graph.Test.prop_num_edges >>
-       quickCheckResult Graph.Test.prop_num_vertices >>
-       quickCheckResult Graph.Test.prop_add_edge >> 
-       quickCheckResult RBTree.Test.prop_rbtree_insert >>
-       quickCheckResult PQ.Test.prop_pq_empty_size_0 >>
-       quickCheckResult PQ.Test.prop_pq_max >>
-       quickCheckResult PQ.Test.prop_pq_delmax >>
-       quickCheckResult PQ.Test.prop_pq_insert >>
-       return ()
+main = do
+    result <- runErrorT (quickCheckResultW Graph.Test.prop_num_edges >>
+                         quickCheckResultW Graph.Test.prop_num_vertices >>
+                         quickCheckResultW Graph.Test.prop_add_edge >> 
+                         quickCheckResultW DGraph.Test.prop_num_edges >>
+                         quickCheckResultW DGraph.Test.prop_num_vertices >>
+                         quickCheckResultW DGraph.Test.prop_add_edge >> 
+                         quickCheckResultW RBTree.Test.prop_rbtree_insert >>
+                         quickCheckResultW PQ.Test.prop_pq_empty_size_0 >>
+                         quickCheckResultW PQ.Test.prop_pq_max >>
+                         quickCheckResultW PQ.Test.prop_pq_delmax >>
+                         quickCheckResultW PQ.Test.prop_pq_insert)
+    case result of
+        Right _      -> exitSuccess
+        Left _       -> exitFailure
